8d2206023056888dfa8cbb643acebf9b3e971b77
Andrew Kerton <andrew.kerton@kogan.com.au>
2022-12-06 16:39:52 +1100

    clean up

--- 6/task1.py
+++ 6/task1.py
@@ -3,35 +3,20 @@ import sys
 import copy
 
 
-def all_unique(item):
-    return len(set(item)) == len(item)
-
-
-length = 14
-
-
-def task1():
-    file1 = open("input.txt", "r")
-    lines = file1.readlines()
-
-    message = lines[0]
+def task(length):
+    message = open("input.txt", "r").readline()
     messageArray = [*message]
 
     for i, char in enumerate(messageArray):
         snippet = messageArray[i : i + length]
 
         if len(snippet) == length and len(set(snippet)) == len(snippet):
-            print("found")
-            print(snippet)
-
-            # index of first + size of block + get next
-            print(i + length)
-
-            break
+            return i + length
 
 
 def main():
-    task1()
+    print("task 1: ", task(4))
+    print("task 2: ", task(14))
 
 
 if __name__ == "__main__":
